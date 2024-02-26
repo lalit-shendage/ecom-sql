0cfa12238101840dd3ba2a28f83c007ee8d4abd9
lalit-shendage <shendage.lalit@gmail.com>
2024-02-26 17:49:01 +0530

    added rate limiter

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const rateLimit = require('express-rate-limit');
+
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
@@ -9,11 +12,19 @@ app.use(express.json());
 
 const db = require('./src/db/connection');
 
-// Routes
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, 
+	limit: 100, 
+	standardHeaders: 'draft-7', 
+	legacyHeaders: false, 
+})
+
 const authRoutes = require('./src/routes/authRoutes');
 const cartRoutes = require('./src/routes/cartRoutes');
 const orderRoutes = require('./src/routes/orderRoutes');
 const productRoutes = require('./src/routes/productRoutes');
+
+app.use(limiter);
 
 app.use('/auth', authRoutes);
 app.use('/cart', cartRoutes);
