948649b4fdb7ee2a661982371381940874d42715
lalit-shendage <shendage.lalit@gmail.com>
2024-02-26 11:26:01 +0530

    added middleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+// authMiddleware.js
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const jwtSecret=process.env.JWT_SECRET;
+
+
+exports.authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized: Missing token' });
+    }
+
+    jwt.verify(token, jwtSecret, (err, user) => {
+        if (err) {
+            return res.status(403).json({ error: 'Forbidden: Invalid token' });
+        }
+        req.user = user;
+        next();
+    });
+};
